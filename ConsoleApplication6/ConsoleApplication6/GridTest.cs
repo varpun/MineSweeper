45f32352f43d78940f5df2849bf9d413e58fb3bb
Varun Punganur <varun.punganur@gmail.com>
2016-01-19 20:19:38 +0800

    adding more Unit tests and updating readme

---
+++ ConsoleApplication6/ConsoleApplication6/GridTest.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace ConsoleApplication6
+{
+    [TestClass()]
+    public class GridTest
+    {
+        private Mock<IGridIO> _GridIO = new Mock<IGridIO>();
+        private string _Message = string.Empty;
+
+        [TestInitialize]
+        public void Initialise()
+        {
+            int[] minePoints = { 1, 1, 2, 1, 3, 1, 4, 1, 5, 1, 6, 1, 7, 1, 8, 1, 1, 2, 2, 2 };
+            var mineQueue = new Queue<int>();
+            foreach (int i in minePoints)
+            {
+                mineQueue.Enqueue(i - 1);
+            }
+            _GridIO.Setup(x => x.GetNextRandomNumber()).Returns(mineQueue.Dequeue);
+            _GridIO.Setup(x => x.WriteLineOutput(It.IsAny<string>())).Callback<string>(r => _Message = r);
+        }
+
+        [TestMethod]
+        public void GridTest_RowAndColumnGreaterThanMax()
+        {
+            Grid testGrid = new Grid(_GridIO.Object);
+            int turn = 0;
+            int[] valuePoints = { 4, 2, 9, 9, 1, 1 };
+            var valueQueue = new Queue<int>();
+            foreach (var i in valuePoints)
+            {
+                valueQueue.Enqueue(i);
+            }
+            _GridIO.Setup(x => x.ReadInputNumber()).Returns(valueQueue.Dequeue);
+
+            bool finish;
+            do
+            {
+                turn++;
+                finish = testGrid.CheckCellPosition(turn);
+                if (!finish)
+                {
+                    testGrid.RevealNeighbors();
+                    finish = testGrid.Win();
+                }
+            }
+            while (!finish);
+            Assert.AreSame("Choose a number between 1 and 8", _Message);
+        }
+
+        [TestMethod]
+        public void GridTest_RowAndColumnRepeated()
+        {
+            Grid testGrid = new Grid(_GridIO.Object);
+            int turn = 0;
+            int[] valuePoints = { 4, 2, 4, 2, 1, 1 };
+            var valueQueue = new Queue<int>();
+            foreach (var i in valuePoints)
+            {
+                valueQueue.Enqueue(i);
+            }
+            _GridIO.Setup(x => x.ReadInputNumber()).Returns(valueQueue.Dequeue);
+
+            bool finish;
+            do
+            {
+                turn++;
+                finish = testGrid.CheckCellPosition(turn);
+                if (!finish)
+                {
+                    testGrid.RevealNeighbors();
+                    finish = testGrid.Win();
+                }
+            }
+            while (!finish);
+            Assert.AreSame("Field already shown", _Message);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # MineSweeper
 Minesweeper Game
+
+This game consists of a 8x8 grid with 10 mines. You can play this game via the console. 
+This game is similar to the traditional minesweeper game. You need to mention the line and 
+the column to unlock the particular cell. If you select a cell which has a mine then you lose the
+game. At the end if you leave 10 cells unexplored which you think might contain the mine then you win the game. 
